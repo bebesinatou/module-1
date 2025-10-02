f89dd63e324e6584f78f953a99bcdce3ebbe2c5c
Sinatou <snguemse@eugeniaschool.com>
2025-10-02 16:27:42 +0000

    premiere partie

---
+++ main.py
@@ -0,0 +1,19 @@
+# TODO: Create an integer variable named 'age' with your age
+
+# TODO: Create a float variable named 'height' with your height in meters
+
+# TODO: Create a string variable named 'name' with your name
+
+# TODO: Create a boolean variable named 'is_student' and set it to True
+
+# Print all variables and their types
+# Example: print(f"Age: {age}, Type: {type(age)}")
+
+AGE = 24
+print(AGE)
+heigth = 1.61
+name = "bebe sinatou"
+is_student = True
+print(name)
+print(heigth)
+print(is_student)
